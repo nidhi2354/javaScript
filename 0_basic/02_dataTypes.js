f05ab6d317e93127e681c29d7fb660f9661b5107
nidhi2354 <147297333+nidhi2354@users.noreply.github.com>
2025-07-10 09:45:29 +0000

    dataTypes

---
+++ 0_basic/02_dataTypes.js
@@ -0,0 +1,13 @@
+// alert(3 + 3) //  we are not using nodejs, not browser
+
+
+// Primitive data types
+// number => 2 to power 53
+//null => standlone value
+//bigint
+//boolean=> true/false
+//string => " "
+//symbol
+// undefined
+// nonPrimitive data types
+  //objects
