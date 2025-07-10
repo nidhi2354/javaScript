99f3cdcd47bf031da1ee975b4456f5e879c0f117
nidhi2354 <147297333+nidhi2354@users.noreply.github.com>
2025-07-10 09:46:15 +0000

    keyword

---
+++ 0_basic/0_1variables.js
@@ -0,0 +1,20 @@
+const account_Id = 12334;
+let accountEmail = "nidhi@gmail.com"
+var accountPassword = "1243"
+accountCity = "jaipur"
+console.log(account_Id);
+// account_Id = 12; // not allowed
+console.log(account_Id)
+
+accountEmail = "softfix@gmail.com"
+ accountPassword = "1200"
+ accountCity = "sitapur"
+
+ console.table([accountEmail,accountPassword,accountCity])
+ /*
+ Prefer not to use var
+ because of issue in block scope and function scope
+ */ 
+
+ 
+
