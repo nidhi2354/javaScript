02aa533400e60faf567cfadc08c57bcad0943e5b
nidhi2354 <147297333+nidhi2354@users.noreply.github.com>
2025-07-10 10:19:58 +0000

    conversionOfDataTypes

---
+++ 0_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = "33abc"
+// console.log(score)
+// console.log (typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof(score))
+// console.log(valueInNumber)
+
+
+
+
+//"33" => 33
+// "33abc" => NAN
+//true => 1; false => 0
+
+
+
+
+// 1 => ture; 0 =>  false
+// "" => false
+// "hitesh" => "true"
+
+
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
