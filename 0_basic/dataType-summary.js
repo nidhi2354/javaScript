fa3611f79bef3b5640f474553fad754e7aa84f3a
nidhi2354 <147297333+nidhi2354@users.noreply.github.com>
2025-07-10 11:28:12 +0000

    dataTypeSummary

---
+++ 0_basic/dataType-summary.js
@@ -0,0 +1,31 @@
+// Primitive dataType
+// 7 types  : string, number, null, undefined, boolean, symbol,bignit
+
+let name = "nidhi";
+console.log(name);
+const id = 22;
+console.log(id);
+let age 
+console.log(age);
+let loggedIn = true;
+console.log(loggedIn);
+
+
+//-------nonPrimitive dataType-----------
+//  array, object, Functions
+
+
+
+//**********type of ********* */
+//undefined - "undefined"
+//null - "object"
+//boolean - "boolean"
+//number - "number" 
+//string - "string"
+//object - "object"
+// function - "function"
+
+//array - "object"
+
+let app = ["apple","banana","guavava"]
+console.log(typeof app)
