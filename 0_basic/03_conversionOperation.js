85e496e8961767c811ef7c58552b85f8d310f8ed
nidhi2354 <147297333+nidhi2354@users.noreply.github.com>
2025-07-10 10:43:49 +0000

    operation

--- 0_basic/03_conversionOperation.js
+++ 0_basic/03_conversionOperation.js
@@ -23,7 +23,17 @@
 
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+
+
+
+//**********************  Operations *************/
+let value = 3
+let negValue = -value
+console.log(negValue)
